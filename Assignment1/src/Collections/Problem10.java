96c8db9d8eef0c913b920079d95202850b2887f4
Ajay Nelluri <84092464+Ajay-Nelluri@users.noreply.github.com>
2022-02-07 23:59:38 -0600

    Adding Assignment01 to the repo

---
+++ Assignment1/src/Collections/Problem10.java
@@ -0,0 +1,53 @@
+package Collections;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Problem10 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		Deque<Character> deque = new LinkedList<>();
+		System.out.println("Enter the no of Characters to insert: ");
+		int n = scan.nextInt();
+		System.out.println("Enter the Character values: ");
+		for (int j = 0; j < n; j++) {
+			deque.addFirst(scan.next().charAt(0));
+		}
+		System.out.println("Enter the no of BinaryNumbers  :");
+		int num = scan.nextInt();
+		int a[] = new int[num];
+		System.out.println("Enter the BinaryNumbers");
+		for (int i = 0; i < num; i++) {
+			a[i] = scan.nextInt();
+		}
+		scan.close();
+		remCharacter(deque, a);
+	}
+
+	public static void remCharacter(Deque<Character> deque, int[] a) {
+
+		char c = 0;
+		String str = "";
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == 1) {
+				c = deque.removeLast();
+			}
+			if (a[i] == 0) {
+				if (c != 0) {
+					deque.addLast(c);
+					c = 0;
+				}
+
+			}
+		}
+		int size = deque.size();
+		for (int i = 0; i < size; i++) {
+			str += deque.removeLast();
+		}
+		System.out.println(str);
+	}
+
+}
---
+++ Assignment1/src/Collections/Problem7.java
@@ -0,0 +1,33 @@
+package Collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Problem7 {
+
+	public static void main(String[] arg) {
+
+		Queue<String> Q = new LinkedList<>();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number of binary elements to be taken to the Queue: ");
+		int a = sc.nextInt();
+		System.out.println("Enter the binary values:");
+		for (int k = 0; k < a; k++) {
+			Q.add(sc.next());
+		}
+		System.out.println("Input (Q): " + Q);
+		System.out.println("Output: " + Problem(Q));
+	}
+
+	public static int Problem(Queue<String> Q) {
+		String String = "";
+		for (String item : Q) {
+			String += item;
+		}
+
+		return Integer.parseInt(String, 2);
+	}
+
+}
